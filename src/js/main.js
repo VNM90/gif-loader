218520e9a91d9096138881f320aa67e671d5bdd7
Arkadiusz Tkaczyk <artek547@gmail.com>
2023-04-02 21:13:24 -0700

    fetch data from Giphy

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,31 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
+import { fetchData } from './api.js';
+
+const searchInput = document.getElementById('search-input');
+const submitBtn = document.getElementById('submit-btn');
+const imageContainer = document.getElementById('image-container');
+
+submitBtn.addEventListener('click', async () => {
+    const searchTerm = searchInput.value;
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=ctyRhySXP0KYCoiC6d37gQ8z96DrZ24A&q=${searchTerm}&limit=10&offset=0&rating=g&lang=en`;
+  
+    try {
+      const response = await fetchData(apiURL);
+      console.log(response);
+      displayImages(response.data)
+
+    } catch (error) {
+      console.error('Error in main.js:', error);
+    }
+  });
+
+function displayImages(imageData) {
+imageContainer.innerHTML = '';
+
+imageData.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image.images.fixed_width.url;
+    imgElement.alt = image.title;
+    imageContainer.appendChild(imgElement);
+});
+}
