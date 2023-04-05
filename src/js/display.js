90e21f9e36c22125a35e8de72baa59f1a286ea08
Arkadiusz Tkaczyk <artek547@gmail.com>
2023-04-04 22:39:40 -0700

    pagination func added

--- src/js/display.js
+++ src/js/display.js
@@ -8,3 +8,5 @@ export function displayImages(imageData, imageContainer) {
         imageContainer.appendChild(imgElement);
     });
     }
+
+ 
--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,45 @@ const submitBtn = document.getElementById('submit-btn');
 const imageContainer = document.getElementById('image-container');
 
 submitBtn.addEventListener('click', async () => {
-    const searchTerm = searchInput.value;
-    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=ctyRhySXP0KYCoiC6d37gQ8z96DrZ24A&q=${searchTerm}&limit=10&offset=0&rating=g&lang=en`;
-  
-    try {
-      const response = await fetchData(apiURL);
-      console.log(response);
-      displayImages(response.data, imageContainer)
+  const searchTerm = searchInput.value;
+  await fetchAndDisplayImages(searchTerm, 0);
+});
 
-    } catch (error) {
-      console.error('Error in main.js:', error);
+async function fetchAndDisplayImages(searchTerm, offset) {
+  const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=ctyRhySXP0KYCoiC6d37gQ8z96DrZ24A&q=${searchTerm}&limit=10&offset=${offset}&rating=g&lang=en`;
+
+  try {
+    const response = await fetchData(apiURL);
+    console.log(response);
+    displayImages(response.data, imageContainer);
+    displayPagination(response.pagination.total_count, searchTerm);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+let itemsPerPage = 10
+let currentPage = 0
+
+function displayPagination(totalResults, searchTerm) {
+  const totalPages = Math.ceil(totalResults / itemsPerPage);
+  const maxPagesToShow = Math.min(totalPages, 10);
+  pagination.innerHTML = '';
+
+  for (let i = 0; i < maxPagesToShow; i++) {
+    const pageBtn = document.createElement('button');
+    pageBtn.innerText = i + 1;
+    pageBtn.classList.add('page-btn');
+    if (i === currentPage) {
+      pageBtn.classList.add('active');
     }
-  });
 
+    pageBtn.addEventListener('click', async () => {
+      currentPage = i;
+      const offset = currentPage * itemsPerPage;
+      await fetchAndDisplayImages(searchTerm, offset);
+    });
+
+    pagination.appendChild(pageBtn);
+  }
+}
