a603fe6facc8f0225797a204d0b5b53d617a9c65
Arkadiusz Tkaczyk <artek547@gmail.com>
2023-04-05 22:48:52 -0700

    pagination function moved to pagination.js file

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import * as bootstrap from 'bootstrap';
 import { fetchData } from './api.js';
 import { displayImages } from './display.js';
+import { displayPagination } from './pagination.js';
+
 
 const searchInput = document.getElementById('search-input');
 const submitBtn = document.getElementById('submit-btn');
@@ -17,35 +19,12 @@ async function fetchAndDisplayImages(searchTerm, offset) {
   try {
     const response = await fetchData(apiURL);
     console.log(response);
+    imageContainer.innerHTML = ''; // Clear the existing images
     displayImages(response.data, imageContainer);
-    displayPagination(response.pagination.total_count, searchTerm);
+    displayPagination(response.pagination.total_count, searchTerm, fetchAndDisplayImages);
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-let itemsPerPage = 10
-let currentPage = 0
 
-function displayPagination(totalResults, searchTerm) {
-  const totalPages = Math.ceil(totalResults / itemsPerPage);
-  const maxPagesToShow = Math.min(totalPages, 10);
-  pagination.innerHTML = '';
-
-  for (let i = 0; i < maxPagesToShow; i++) {
-    const pageBtn = document.createElement('button');
-    pageBtn.innerText = i + 1;
-    pageBtn.classList.add('page-btn');
-    if (i === currentPage) {
-      pageBtn.classList.add('active');
-    }
-
-    pageBtn.addEventListener('click', async () => {
-      currentPage = i;
-      const offset = currentPage * itemsPerPage;
-      await fetchAndDisplayImages(searchTerm, offset);
-    });
-
-    pagination.appendChild(pageBtn);
-  }
-}
---
+++ src/js/pagination.js
@@ -0,0 +1,25 @@
+let itemsPerPage = 10
+let currentPage = 0
+
+export function displayPagination(totalResults, searchTerm, fetchAndDisplayImagesCallback ) {
+  const totalPages = Math.ceil(totalResults / itemsPerPage);
+  const maxPagesToShow = Math.min(totalPages, 10);
+  pagination.innerHTML = '';
+
+  for (let i = 0; i < maxPagesToShow; i++) {
+    const pageBtn = document.createElement('button');
+    pageBtn.innerText = i + 1;
+    pageBtn.classList.add('page-btn');
+    if (i === currentPage) {
+      pageBtn.classList.add('active');
+    }
+
+    pageBtn.addEventListener('click', async () => {
+      currentPage = i;
+      const offset = currentPage * itemsPerPage;
+      await fetchAndDisplayImagesCallback(searchTerm, offset);
+    });
+
+    pagination.appendChild(pageBtn);
+  }
+}
